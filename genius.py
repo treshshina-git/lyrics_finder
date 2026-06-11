cfcb0e2b7bec6384d749b651eb1afe2687361045
treshshina-git <treshshina@gmail.com>
2026-06-11 19:05:18 +0200

    Update genius.py

--- genius.py
+++ genius.py
@@ -3,39 +3,41 @@ import os
 
 GENIUS_TOKEN = os.getenv("GENIUS_TOKEN")
 
-
 async def search_song(query: str):
+
     url = "https://api.genius.com/search"
 
     headers = {
         "Authorization": f"Bearer {GENIUS_TOKEN}"
     }
 
-    params = {
-        "q": query
-    }
-
     async with aiohttp.ClientSession() as session:
+
         async with session.get(
             url,
             headers=headers,
-            params=params
+            params={"q": query}
         ) as response:
-
-            if response.status != 200:
-                return None
 
             data = await response.json()
 
             hits = data["response"]["hits"]
 
             if not hits:
-                return None
+                return []
 
-            song = hits[0]["result"]
+            results = []
 
-            return {
-                "title": song["title"],
-                "artist": song["primary_artist"]["name"],
-                "url": song["url"]
-            }
+            for hit in hits[:10]:
+
+                song = hit["result"]
+
+                results.append({
+                    "title": song["title"],
+                    "artist": song["primary_artist"]["name"],
+                    "url": song["url"],
+                    "id": song["id"]
+                })
+
+            return results
+
