bebc57289e10eb1a77aded849684521445044425
treshshina-git <treshshina@gmail.com>
2026-06-11 18:04:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,12 +17,12 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
+
 @dp.message(CommandStart())
 async def start(message: Message):
 
-    await message.answer(
-        "🎵 Отправьте название песни или строку из песни."
-    )
+    await message.answer("🎵 Отправьте название песни или строку из песни.")
+
 
 @dp.message()
 async def find_song(message: Message):
@@ -30,53 +30,42 @@ async def find_song(message: Message):
     query = message.text.strip()
 
     if len(query) > 200:
-        await message.answer(
-            "Слишком длинный запрос."
-        )
+        await message.answer("Слишком длинный запрос.")
         return
 
-    status = await message.answer(
-        "🔍 Ищу песню..."
-    )
+    status = await message.answer("🔍 Ищу песню...")
 
     song = await search_song(query)
     print(song)
     if not song:
-        await status.edit_text(
-            "❌ Песня не найдена."
-        )
+        await status.edit_text("❌ Песня не найдена.")
         return
     artist = song["artist"]
     title = song["title"]
 
-    lyrics = await get_lyrics(
-        artist,
-        title
-    )
-    
+    lyrics = await get_lyrics(artist, title)
+
     if not lyrics:
-        await message.answer(
-           f"🎵 {artist} - {title}\n\n"
-           f"Текст не найден в LRCLIB.\n"
-           f"Открыть Genius:\n{url}"
-        )
+        await message.answer(f"🎵 {artist} - {title}\n\n"
+                             f"Текст не найден в LRCLIB.\n"
+                             f"Открыть Genius:\n{url}")
         return
-        await status.edit_text(
-            f"✅ Найдено:\n"
-            f"{song['artist']} - {song['title']}"
-        )
-        
+        await status.edit_text(f"✅ Найдено:\n"
+                               f"{song['artist']} - {song['title']}")
+
         def clean_title(title):
             title = re.sub(r"\(.*?\)", "", title)
             title = re.sub(r"\[.*?\]", "", title)
             return title.strip()
-            
+
         def clean_artist(artist):
             artist = re.sub(r"\(.*?\)", "", artist)
+
         return artist.strip()
-        
+
         for part in split_text(lyrics):
             await message.answer(part)
+
 
 async def main():
     await dp.start_polling(bot)
