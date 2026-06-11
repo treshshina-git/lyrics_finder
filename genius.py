828b12a45fa52d57e86483685af00cb9424685f3
treshshina-git <treshshina@gmail.com>
2026-06-11 17:18:02 +0200

    Update genius.py

--- genius.py
+++ genius.py
@@ -1,20 +1,40 @@
-import lyricsgenius
+import aiohttp
 import os
 
-genius = lyricsgenius.Genius(
-    os.getenv("GENIUS_TOKEN")
-)
+GENIUS_TOKEN = os.getenv("GENIUS_TOKEN")
 
 
-def search_song(query):
+async def search_song(query: str):
+    url = "https://api.genius.com/search"
 
-    song = genius.search_song(query)
-
-    if not song:
-        return None
-
-    return {
-        "title": song.title,
-        "artist": song.artist,
-        "lyrics": song.lyrics
+    headers = {
+        "Authorization": f"Bearer {GENIUS_TOKEN}"
     }
+
+    params = {
+        "q": query
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            url,
+            headers=headers,
+            params=params
+        ) as response:
+
+            if response.status != 200:
+                return None
+
+            data = await response.json()
+
+            hits = data["response"]["hits"]
+
+            if not hits:
+                return None
+
+            song = hits[0]["result"]
+
+            return {
+                "title": song["title"],
+                "artist": song["primary_artist"]["name"]
+            }
